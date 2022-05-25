b2aa2019d559d3d1da89e1c5a391c0f80595a5f4
Panagiotis <panfanartzis@gmail.com>
2022-05-25 12:20:16 +0300

    buySellStock

---
+++ buy_sell_stock/index.php
@@ -0,0 +1,18 @@
+<?php
+class Solution
+{
+    function maxProfit($prices)
+    {
+        $count = count($prices);
+        $minPrice = $prices[0];
+        $profit = 0;
+        for ($i = 0; $i < $count; $i++) {
+            if ($prices[$i] < $minPrice) {
+                $minPrice = $prices[$i];
+                continue;
+            }
+            if ($prices[$i] - $minPrice > $profit) $profit = $prices[$i] - $minPrice;
+        }
+        return $profit;
+    }
+}
